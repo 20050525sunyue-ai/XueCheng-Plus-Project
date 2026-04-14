568d52054069c9304e541aebde49c033074b3534
Shang <3506474986@qq.com>
2026-04-14 15:42:47 +0800

    提交分支

---
+++ src/main/java/com/xuecheng/XueChengPlusApplication.java
@@ -0,0 +1,16 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 学成在线Plus项目启动类
+ */
+@SpringBootApplication
+public class XueChengPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XueChengPlusApplication.class, args);
+    }
+
+}
